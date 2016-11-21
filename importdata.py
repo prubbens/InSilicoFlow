0c2eec568831849cbff6f52d81e17d4cbe0aaab6
prubbens <peter.rubbens@ugent.be>
2016-11-21 15:31:13 +0100

    Update importdata.py

--- importdata.py
+++ importdata.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 
 import fcsparser
 import pandas as pd
 import os 
 
 '''Import data files'''
-for file in os.listdir("Growth Curve/filtered/FCS/Cells"):
+for file in os.listdir("Directory"):
     if file.endswith(".fcs"):
-        dummystring = str('Growth Curve/filtered/FCS/Cells/' + file)
+        dummystring = str('Directory/' + file)
         meta, data = fcsparser.parse(dummystring, meta_data_only=False, reformat_meta=False)
         file = file[:-4]
         pd_data = pd.DataFrame(data)
-        dummystring2 = str("Growth Curve/filtered/CSV/Cells/" + file + ".csv")
+        dummystring2 = str("Directory/" + file + ".csv")
         pd_data.to_csv(dummystring2)
         print(file)
